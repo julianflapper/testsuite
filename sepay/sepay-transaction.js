d2d72fceb6a0929ff3ee5cf5e199cbf7ddcd6a80
Julian Flapper <julian@allestogo.nl>
2024-11-22 13:22:08 +0100

    Add debugs

--- sepay/sepay-transaction.js
+++ sepay/sepay-transaction.js
@@ -81,7 +81,7 @@ class SepayClient {
 
   sendData(data) {
     return new Promise((resolve, reject) => {
-      this.client.on("data", (data) => {
+      this.client.once("data", (data) => {
         console.log("Received Packet: ", data);
         resolve(data);
       });
